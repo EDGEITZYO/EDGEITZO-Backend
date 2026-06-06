28c0454e94a45a409bf3bfd9e103e9fed7f8e5fd
reeyr <yuri12120771@gmail.com>
2026-06-07 01:13:25 +0900

    feat: add localhost:5173 to CORS allow_origins

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.frontend_url],
+    allow_origins=[settings.frontend_url, "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
