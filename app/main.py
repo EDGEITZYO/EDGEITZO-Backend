85d22e2d27c4d653067c2c3342cad533e8256bf4
reeyr <yuri12120771@gmail.com>
2026-06-13 01:00:57 +0900

    feat: CORS 허용 origin에 Vercel 프리뷰 URL 추가

--- app/main.py
+++ app/main.py
@@ -40,7 +40,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.frontend_url, "http://localhost:5173", "http://localhost:5173/"],
+    allow_origins=[settings.frontend_url, "http://localhost:5173", "http://localhost:5173/", "https://edgeitzo-frontend-git-dev-minju3212s-projects.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
